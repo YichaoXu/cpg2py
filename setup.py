e1c7e7fa6af737295ec29640f5b0085e7fa69b28
Yichao Xu <yxu166@jh.edu>
2025-02-19 18:59:57 -0500

    Update .gitignore and enhance pyproject.toml with project metadata

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cpg2py",  # 你的包名
-    version="1.0.0",  # 版本号
+    name="cpg2py",  
+    version="1.0.0", 
     author="Yichao Xu",
     author_email="yxu166@jhu.edu",
     description="A graph-based data structure designed for querying CSV files in Joern format in Python",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/YichaoXu/cpg2py",  # GitHub 项目地址
+    url="https://github.com/YichaoXu/cpg2py",  
     packages=find_packages(),
-    install_requires=[],  # 你的包的依赖项（如果有）
+    install_requires=[],  
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
