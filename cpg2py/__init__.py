08ab392ed8566a31d7b69d6611394b1520a6e6d2
Yichao Xu <yxu166@jh.edu>
2025-02-20 22:06:49 -0500

    Add verbose option to cpg_graph for enhanced logging of warnings

--- cpg2py/__init__.py
+++ cpg2py/__init__.py
@@ -3,16 +3,16 @@ from pathlib import Path
 from .cpg import _Graph
 from .abc import *
 
-def cpg_graph(node_csv: Path, edge_csv: Path) -> "_Graph":
+def cpg_graph(node_csv: Path, edge_csv: Path, verbose: bool = False) -> "_Graph":
     storage = Storage()
     with open(node_csv, 'r') as n_file: 
         reader = DictReader(n_file, delimiter='\t')
         for node_props in reader: 
             nid = node_props.get("id:int", None) 
             if nid is None: node_props.get("id") 
-            if not storage.add_node(nid): 
+            if not storage.add_node(nid) and verbose: 
                 print(f"WARN: Node {nid} already exists in the graph")
-            if not storage.set_node_props(nid, node_props): 
+            if not storage.set_node_props(nid, node_props) and verbose: 
                 print(f"WARN: Failed to set properties for node {nid}")
     with open(edge_csv, 'r') as f:
         reader = DictReader(f, delimiter='\t')
@@ -26,14 +26,14 @@ def cpg_graph(node_csv: Path, edge_csv: Path) -> "_Graph":
             edge_id = (f_nid, t_nid, e_type)
             if not storage.contains_node(edge_id[0]): 
                 storage.add_node(edge_id[0])
-                print(f"WARN: node {edge_id[0]} does not exists")
+                if verbose: print(f"WARN: node {edge_id[0]} does not exists")
             if not storage.contains_node(edge_id[1]): 
                 storage.add_node(edge_id[1])
-                print(f"WARN: node {edge_id[1]} does not exists")
+                if verbose: print(f"WARN: node {edge_id[1]} does not exists")
             if not storage.add_edge(edge_id): 
-                print(f"WARN: Edge {f_nid} -> {t_nid} already exists in the graph")
+                if verbose: print(f"WARN: Edge {f_nid} -> {t_nid} already exists in the graph")
             if not storage.set_edge_props(edge_id, edge_props): 
-                print(f"WARN: Failed to set properties for edge {edge_id}")
+                if verbose: print(f"WARN: Failed to set properties for edge {edge_id}")
         return _Graph(storage)
 
 
